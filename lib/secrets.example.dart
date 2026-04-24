435b7995b0f13252ca207fd93c13f6a42016e4d0
Sam Hedin <sam.hedin@gmail.com>
2026-04-24 16:04:16 +0200

    Move FCM license keys to secrets.dart

---
+++ lib/secrets.example.dart
@@ -0,0 +1,8 @@
+const String revenueCatAndroid = 'your_revenuecat_android_key';
+const String revenueCatIos = 'your_revenuecat_ios_key';
+const String revenueCatWeb = 'your_revenuecat_web_key';
+
+const List<String> fcmLicenseKeys = [
+  'your_fcm_license_key_1',
+  'your_fcm_license_key_2',
+];
