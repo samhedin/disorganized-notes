52c5d1373ffdb78f428445d60369cba4acecefe3
Sam Hedin <sam.hedin@gmail.com>
2026-04-25 21:27:44 +0200

    ios and web builds

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,16 +1,32 @@
 import Flutter
 import UIKit
+import awesome_notifications
+import shared_preferences_foundation
+import UserNotifications
+import alarm
 
+//https://stackoverflow.com/a/75264461
 @main
-@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
+@objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+      if #available(iOS 10.0, *) {
+  UNUserNotificationCenter.current().delegate = self as UNUserNotificationCenterDelegate
+}
+      SwiftAlarmPlugin.registerBackgroundTasks()
+      
+      GeneratedPluginRegistrant.register(with: self)
+      
+      SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in          
+          SwiftAwesomeNotificationsPlugin.register(
+            with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)          
+          SharedPreferencesPlugin.register(
+            with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin")!)
+      }
 
-  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
-    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+           
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
